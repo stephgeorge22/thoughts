c554321f952100d138c7fde3fcdb4e39278692f3
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-08-13 00:11:14 -0400

    reaction schema and virtual

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,34 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+    {
+        // custom id 
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+)
 
 const ThoughtSchema = new Schema({
     thoughtText: {
@@ -14,10 +43,24 @@ const ThoughtSchema = new Schema({
         get: createdAtVal => dateFormat(createdAtVal)
     },
     username: {
-        type: String, 
+        type: String,
         required: true
+    },
+    reactions: [ReactionSchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
-})
+);
+
+// virtual for total length of thoughts
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
 
 // create the User model using the UserSchema
 const Thought = model('Thought', ThoughtSchema);
